9d429ce015e5439e5d8f3aace9acb8346b45eb25
michael thomas <michael.thomas2014@gmail.com>
2020-05-08 05:34:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,11 @@ def countdown(secs)
   "HAPPY NEW YEAR!"
 end
   
+def countdown_with_sleep(secs)
+  time = secs
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
